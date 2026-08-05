5347e488fe5307667e778d7db30f625233806a20
LiYingJie-LiL <3319802465@qq.com>
2026-08-05 22:49:34 +0800

    公共字段自动填充模块

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -27,6 +29,7 @@ public interface EmployeeMapper {
     @Insert("insert into employee(name,username,password,phone,sex,id_number,create_time,update_time,create_user," +
             "update_user,status) values " +
             "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})" )
+   @AutoFill(value = OperationType.INSERT)
     void insert(Employee employee);
 
 
@@ -43,6 +46,7 @@ public interface EmployeeMapper {
      */
     //@Update("update employee set status=#{status} where id=#{id}")
     //但是利用@Update注解对于修改其他的属性并不是很通用，所有这里用到动态SQL语句
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
     /**
