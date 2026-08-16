187585228d3b8d60f83db754a28b1b7d26400802
one <3356492943@qq.com>
2026-08-16 14:50:10 +0800

    商品浏览业务代码

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -47,4 +47,11 @@ public interface DishService {
      * @return
      */
     List<Dish> list(Long categiryId);
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
